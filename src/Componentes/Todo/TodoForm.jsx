1db978c670bc055277bdc40bc0b50ca2bbb5acb5
Master <masterzarby@gmail.com>
2025-01-19 17:16:13 -0400

    Algumas Alterações

--- src/Componentes/Todo/TodoForm.jsx
+++ src/Componentes/Todo/TodoForm.jsx
@@ -3,18 +3,16 @@ import React from "react";
 const TodoForm = ({ addTodo }) => {
   const [text, setText] = React.useState("");
   const [category, setCategory] = React.useState("");
-  const [customCategory, setCustomCategory] = React.useState("");
-
+  const [otherCategory, setOtherCategory] = React.useState("");
   const handleSubmit = event => {
     event.preventDefault();
-    const finalCategory = category === "Custom" ? customCategory : category;
-    if (text && finalCategory && finalCategory !== "Select") {
-      addTodo(text, finalCategory);
+    const finalCat = category === "Outro" ? otherCategory : category;
+    if (text && finalCat && finalCat !== "Select") {
+      addTodo(text, finalCat);
       setText("");
       setCategory("");
-      setCustomCategory("");
     } else {
-      alert("Por favor, preencha todos os campos");
+      alert("Porfavor, preencha todos os campos");
     }
   };
 
@@ -35,14 +33,14 @@ const TodoForm = ({ addTodo }) => {
           <option value="Select">Selecionar Categoria</option>
           <option value="Frontend">Frontend</option>
           <option value="Backend">Backend</option>
-          <option value="Custom">Outra (Especificar abaixo)</option>
+          <option value="Outro">Outra (Especificar abaixo)</option>
         </select>
-        {category === "Custom" &&
+        {category === "Outro" &&
           <input
             type="text"
-            placeholder="Especificar categoria"
-            value={customCategory}
-            onChange={event => setCustomCategory(event.target.value)}
+            placeholder="Especificar Categoria"
+            value={otherCategory}
+            onChange={event => setOtherCategory(event.target.value)}
           />}
         <button type="submit">Adicionar</button>
       </form>
